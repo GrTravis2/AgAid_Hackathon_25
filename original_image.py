de4550743cfb290494734638314f743d5a40a3a9
Gavin Travis <g.r.travis.2@gmail.com>
2025-02-01 18:31:10 -0800

    location is enum instead of str

--- original_image.py
+++ original_image.py
@@ -29,7 +29,7 @@ class original_image:
 
     def __init__(self, abs_filepath: str):
         self.file_path: str = abs_filepath[abs_filepath.find("original/", )+9:]
-        self.location: str = ""
+        self.location: int = -1
         self.image_num: int = -1
         self.part_num: int = -1
 
@@ -39,7 +39,7 @@ class original_image:
         self.data = self.agAid_image_loader(abs_filepath)
 
         #add new columns with classification labels
-        self.data["location"] = self.location
+        self.data["location"] = self.map_location(self.location)
         self.data["image_num"] = self.image_num
         self.data["part_num"] = self.part_num
 
@@ -53,12 +53,32 @@ class original_image:
         
         self.data = self.data.join(residue_df)
 
-        #print(self.data)
+        print(self.data)
+
+    def map_location(self, location: str) -> int:
+        location_value: int = -1
+        match location:
+            case "Zak-W-winterBarley_1m_20220401":
+                location_value = 0
+            case "Ritzville2-SprWheat1m20220329":
+                location_value = 1
+            case "Ritzville3-WheatFallow1pass1m20220329":
+                location_value = 2
+            case "Ritzville6-SprWheatWintPeas1m20220329":
+                location_value = 3
+            case "Limbaugh1-1m20220328":
+                location_value = 4
+            case _:
+                assert(False)
+
+        return location_value
+            
+
 
     
     def parse_filename_by_location(self, filepath: str) -> tuple[str, int, int]:
         file: list[str] = filepath.split("/")
-        location: str = file[0]
+        location: int = file[0]
         filename: str = file[1]
         start: int = filename.find("IMG_") + 4
         end: int = -1
